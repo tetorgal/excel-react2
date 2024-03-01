74997882b1be178b15e16c7b78b35ea425f1db6c
tetorgal <hector.galarza.css@gmail.com>
2024-03-01 02:47:46 -0600

    Instalar axios, implementar cors en backend, cambiar peticion a axios para frontend

--- client/src/components/AdmAsistencia.js
+++ client/src/components/AdmAsistencia.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardBody, CardHeader, CardTitle, Container, Table } from "reactstrap";
+import axios from "axios"; // Importar axios para hacer la solicitud HTTP
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Container,
+  Table,
+} from "reactstrap";
 import BotonExcelDefault from "./Asistencia/BotonExcelDefault";
 import BotonExcelEstilizado from "./Asistencia/BotonExcelEstilizado";
 
@@ -7,31 +15,14 @@ const AdmAsistencia = () => {
   const [asistencia, setAsistencia] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/asistencias/datos-asistencias")
-      .then((res) => res.json())
-      .then((json) => {
-        let listaAsistencia = json.products.map((data) => {
-          return {
-            no_asistencia: data.asistencia,
-            nombre: data.nombre,
-            uno: data.uno,
-            dos: data.dos,
-            tres: data.tres,
-            cuatro: data.cuatro,
-            cinco: data.cinco,
-            seis: data.seis,
-            siete: data.siete,
-            ocho: data.ocho,
-            nueve: data.nueve,
-            diez: data.diez,
-            once: data.once,
-            doce: data.doce,
-            trece: data.trece,
-            catorce: data.catorce,
-            quince: data.quince,
-          };
-        });
-        setAsistencia(listaAsistencia);
+    axios
+      .get("http://127.0.0.1:5000/asistencias/get-asistencias") // Hacer la solicitud HTTP usando axios
+      .then((response) => {
+        console.log("si obtuvo respuesta de la peticion");
+        setAsistencia(response.data); // Establecer los datos de asistencia en el estado
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos de asistencia:", error);
       });
   }, []);
 
@@ -43,7 +34,7 @@ const AdmAsistencia = () => {
             <CardTitle>Lista de Asistencia</CardTitle>
             <div>
               <BotonExcelDefault asistencia={asistencia} />
-              <BotonExcelEstilizado qsistencia={asistencia} />
+              <BotonExcelEstilizado asistencia={asistencia} />
             </div>
           </div>
         </CardHeader>
@@ -52,22 +43,17 @@ const AdmAsistencia = () => {
             <thead className="bg-primary text-white">
               <tr>
                 <th>No. Asistencia</th>
-                <th>Nombre</th>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>10</th>
-                <th>11</th>
-                <th>12</th>
-                <th>13</th>
-                <th>14</th>
-                <th>15</th>
+                <th>Nombre y Apellido</th>
+                <th>Dia 1</th>
+                <th>Dia 2</th>
+                <th>Dia 3</th>
+                <th>Dia 4</th>
+                <th>Dia 5</th>
+                <th>Dia 6</th>
+                <th>Dia 7</th>
+                <th>Dia 8</th>
+                <th>Dia 9</th>
+                <th>Dia 10</th>
               </tr>
             </thead>
             <tbody>
@@ -75,22 +61,19 @@ const AdmAsistencia = () => {
                 return (
                   <tr key={asistencia.id}>
                     <td>{asistencia.id}</td>
-                    <td>{asistencia.nombre}</td>
-                    <td>{asistencia.uno}</td>
-                    <td>{asistencia.dos}</td>
-                    <td>{asistencia.tres}</td>
-                    <td>{asistencia.cuatro}</td>
-                    <td>{asistencia.cinco}</td>
-                    <td>{asistencia.seis}</td>
-                    <td>{asistencia.siete}</td>
-                    <td>{asistencia.ocho}</td>
-                    <td>{asistencia.nueve}</td>
-                    <td>{asistencia.diez}</td>
-                    <td>{asistencia.once}</td>
-                    <td>{asistencia.doce}</td>
-                    <td>{asistencia.trece}</td>
-                    <td>{asistencia.catorce}</td>
-                    <td>{asistencia.quince}</td>
+                    <td>
+                      {asistencia.apellido} {asistencia.nombre}
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                   </tr>
                 );
               })}
--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,19 @@ const attendanceRoutes = require("./routes/attendanceRoutes");
 
 // Middleware para manejar el cuerpo de las solicitudes
 app.use(express.json());
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS" // Métodos HTTP permitidos
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization" // Encabezados permitidos en las solicitudes
+  );
+  next();
+});
 
 // Middleware para manejar las rutas relacionadas con las asistencias
 app.use("/asistencias", attendanceRoutes);
